e77652d8eb1c57968e22807afede91940ea6b767
josepereza <josepereza66@gmail.com>
2021-06-20 11:12:33 +0200

    first3 commit

--- src/app/clientes/pages/formulario/formulario.component.ts
+++ src/app/clientes/pages/formulario/formulario.component.ts
@@ -10,39 +10,40 @@ import { ClientesService } from '../../services/clientes.service';
 export class FormularioComponent implements OnInit {
   profileForm = new FormGroup({
     id: new FormControl(''),
-    nombre: new FormControl('',[Validators.required]),
-    saldo:new FormControl('')
+    nombre: new FormControl('', [Validators.required]),
+    saldo: new FormControl('')
   });
-  actualizar:boolean=false;
-  constructor(private http:HttpClient,private clienteService:ClientesService) { }
-cliente:any=''
+  actualizar: boolean = false;
+  constructor(private http: HttpClient, private clienteService: ClientesService) { }
+  cliente: any = ''
   ngOnInit(): void {
-    
+
   }
-enviar(){
-if (this.profileForm.valid){
-  if (this.actualizar==true){
-    this.clienteService.actualizarCliente(this.profileForm.value.id, this.profileForm.value)
-    .subscribe(data=>{ this.actualizar=false; this.cliente=data;
-      this.profileForm.reset()
-      
+  enviar() {
+    if (this.profileForm.valid) {
+      if (this.actualizar == true) {
+        this.clienteService.actualizarCliente(this.profileForm.value.id, this.profileForm.value)
+          .subscribe(data => {
+            this.actualizar = false; this.cliente = data;
+            this.profileForm.reset()
+
+          })
+      } else {
+        console.log('este es el else')
+        this.clienteService.addCliente(this.profileForm.value)
+          .subscribe((data: any) => this.cliente = data)
+      }
+    } else return
+  }
+
+
+  nuevoCliente(data: any) {
+    this.actualizar = true;
+    console.log(data)
+    this.profileForm.patchValue({
+      id: data.id,
+      nombre: data.nombre,
+      saldo: data.saldo
     })
-  }else {
-  console.log ('este es el else')
-  this.clienteService.addCliente(this.profileForm.value)
-  .subscribe((data:any)=>this.cliente=data)
-}
-}else return
-}
-
-
-nuevoCliente(data:any){
-  this.actualizar=true;
-console.log(data)
-this.profileForm.patchValue({
-  id:data.id,
-  nombre:data.nombre,
-  saldo:data.saldo
-})
-}
+  }
 }
